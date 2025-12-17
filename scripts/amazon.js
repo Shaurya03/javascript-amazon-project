d49862529aec33c180ae27ede14b4f18cfdf342e
Shaurya03 <shauryachaudhary2003@gmail.com>
2025-12-17 22:29:57 +0530

    Create the quantity selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -69,13 +69,18 @@ document.querySelectorAll('.js-add-to-cart')
           matchingItem = item;
         } 
       });
+
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+
+      const quantity = Number(quantitySelector.value);
+      console.log(quantity);
       
       if (matchingItem) {
-        matchingItem.quantity++;
+        matchingItem.quantity += quantity;
       } else {
          cart.push({
          productId: productId,
-         quantity: 1
+         quantity: quantity
         });
       }
 
