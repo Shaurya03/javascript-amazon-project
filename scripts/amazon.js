2e34c42e33b960dd365a7a7ca4ceaf9931cd6f24
Shaurya03 <shauryachaudhary2003@gmail.com>
2025-12-19 11:01:48 +0530

    Update the cart quantity in amazon.html and checkout.html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -62,15 +62,18 @@ document.querySelector('.js-products-grid')
   .innerHTML = productsHTML;
 
 function updateCartQuantity() {
-   let cartQuantity = 0;
+   const cartQuantity = calculateCartQuantity();
 
-    cart.forEach((cartItem) => {
-      cartQuantity += cartItem.quantity;
-    });
-
-    document.querySelector('.js-cart-quantity')
-      .innerHTML = cartQuantity;
+    if (cartQuantity === 0) {
+      document.querySelector('.js-cart-quantity')
+    .innerHTML = '';
+    } else {
+      document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+    }
 }
+
+updateCartQuantity();
 
 function addedToCartMessage(addedMessageTimeoutId, productId) {
   const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
