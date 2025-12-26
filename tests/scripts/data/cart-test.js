100fd3df1f8679d858ab7df48edd89c1fc79585b
Shaurya03 <shauryachaudhary2003@gmail.com>
2025-12-26 23:05:03 +0530

    Create automated tests for removeFromCart function

--- tests/scripts/data/cart-test.js
+++ tests/scripts/data/cart-test.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage } from "../../../scripts/data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart } from "../../../scripts/data/cart.js";
 
 describe('Test Suite: addToCart', () => {
   beforeEach(() => {
@@ -38,6 +38,40 @@ describe('Test Suite: addToCart', () => {
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].quantity).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1,
+      deliveryOptionId: '1'
+    }]));
+  });
+});
+
+describe('Test Suite: removeFromCart', () => {
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+  });
+
+  it('remove a product that is in the cart', () => {
+    removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.length).toEqual(0);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([]));
+  });
+
+  it('remove a product that is not in the cart', () => {
+    removeFromCart('something');
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity: 1,
