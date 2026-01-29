ed7238000b920069875b41b20638ad976b8747b9
Shaurya03 <shauryachaudhary2003@gmail.com>
2026-01-29 10:56:42 +0530

    Convert loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,24 +1,19 @@
 import { renderCheckoutHeader } from "./checkout/checkout-header.js";
 import { renderOrderSummary } from "./checkout/order-summary.js";
 import { renderPaymentSummary } from "./checkout/payment-summary.js";
-import { loadProducts } from "./data/products.js";
+import { loadProducts, loadProductsFetch } from "./data/products.js";
 import { loadCart } from "./data/cart.js";
 // import './data/cart-class.js';
 
 Promise.all([
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve('value1');
-    });
-  }),
+  loadProductsFetch(),
   new Promise((resolve) => {
     loadCart(() => {
       resolve();
     });
   })
 
-]).then((values) => {
-    console.log(values);
+]).then(() => {
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
--- scripts/data/products.js
+++ scripts/data/products.js
@@ -88,6 +88,30 @@ function isAppliance(productDetails) {
   return productDetails.keywords?.includes('appliances');
 }
 
+export function loadProductsFetch () {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      
+      if (isAppliance(productDetails)){
+        return new Appliance(productDetails);
+      }
+      
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });
+
+  return promise;
+}
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
